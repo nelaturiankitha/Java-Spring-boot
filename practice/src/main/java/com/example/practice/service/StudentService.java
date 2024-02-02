c0993f99d9dc0f5aec2fc0c7d065765fc3f1ba13
Ankitha Nelaturi <nelaturiankitha@gmail.com>
2024-02-02 02:29:21 +0000

    Reapply "adding function in service class"
    This reverts commit c20cc157e26e919a55db3a729689e9fda1eeeaef.
    reverting to pull

--- practice/src/main/java/com/example/practice/service/StudentService.java
+++ practice/src/main/java/com/example/practice/service/StudentService.java
@@ -1,4 +1,23 @@
 package com.example.practice.service;
 
+import com.example.practice.model.Student;
+import com.example.practice.repo.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public Optional<Student> getUser(Integer id) {
+        return studentRepository.findById(Long.valueOf(id));
+    }
 }
